23dc8a29bd52ad4781dbada359aecafd674bd2e6
tranvinhthuy <tranvinhthuybk@gmail.com>
2018-12-23 10:19:41 +0100

    Change job TPCH18 name to TPCH

--- job_group_data.py
+++ job_group_data.py
@@ -2,7 +2,7 @@ class JobGroupData:
     group1 = ["WordCount", "KMeans", "LinearRegression"]
     group2 = ["LogisticRegression", "SVM"]
     group3 = ["SortedWordCount", "PageRank"]
-    group4 = ["TPCH18"]
+    group4 = ["TPCH"]
     group5 = ["Sort"]
     group6 = ["ConnectedComponent"]
     groups = [group1, group2, group3, group4, group5, group6]
@@ -10,12 +10,12 @@ class JobGroupData:
     groupIndexes = {"WordCount": 0, "KMeans": 0, "LinearRegression": 0,
                     "LogisticRegression": 1, "SVM": 1,
                     "SortedWordCount": 2, "PageRank": 2,
-                    "TPCH18": 3,
+                    "TPCH": 3,
                     "Sort": 4,
                     "ConnectedComponent": 5}
     group_names = ["WC,KM,LiR", "LoR,SVM", "SWC,PR", "TPCH", "S", "CC"]
 
-    def get_group_name(group_index):
+    def get_group_name(self, group_index):
         return self.group_names[group_index]
 
     # Cluster slots
